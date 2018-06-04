901b1bdb08363d4915789adee9cd9fa788441d94
vvhuang <loveuu715@163.com>
2018-06-04 16:23:31 +0800

    first commit.

---
+++ library/src/main/java/com/vvme/vok/Vok.java
@@ -0,0 +1,11 @@
+package com.vvme.vok;
+
+/**
+ * Project name:Vok
+ * Author:VV
+ * Created on 2018/6/4 16:23.
+ * Copyright (c) 2018, vvismile@163.com All Rights Reserved.
+ * Description: TODO
+ */
+public class Vok {
+}
