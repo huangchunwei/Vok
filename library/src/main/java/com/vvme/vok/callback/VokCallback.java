f2c36a49f7afe9b71525e695a6f8c2d8c5f1ca39
vvhuang <loveuu715@163.com>
2018-06-04 16:27:04 +0800

    first commit.

---
+++ library/src/main/java/com/vvme/vok/callback/VokCallback.java
@@ -0,0 +1,11 @@
+package com.vvme.vok.callback;
+
+/**
+ * Project name:Vok
+ * Author:VV
+ * Created on 2018/6/4 16:25.
+ * Copyright (c) 2018, vvismile@163.com All Rights Reserved.
+ * Description: TODO
+ */
+public class VokCallback {
+}
---
+++ library/src/main/java/com/vvme/vok/convert/ResponseConvert.java
@@ -0,0 +1,11 @@
+package com.vvme.vok.convert;
+
+/**
+ * Project name:Vok
+ * Author:VV
+ * Created on 2018/6/4 16:26.
+ * Copyright (c) 2018, vvismile@163.com All Rights Reserved.
+ * Description: TODO
+ */
+public class ResponseConvert {
+}
